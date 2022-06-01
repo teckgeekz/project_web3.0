25a33be0069223ef1ea908985639855343401264
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2022-06-02 04:12:10 +0530

    updated contract addr
    updated contract addr

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x5160D3747d82e7590279BE856ca637373A1B2119";
 export const contractABI = abi.abi;
